e0ecbbcf0e351b8983610f305143bdcb19de247c
siddhi_parkar <siddhiparkar15@gmail.com>
2017-06-18 17:29:53 +0530

    compilation

--- src/main/java/com/connectin/business/post/controller/PostController.java
+++ src/main/java/com/connectin/business/post/controller/PostController.java
@@ -4,14 +4,12 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.connectin.business.post.entity.Post;
 import com.connectin.business.post.service.PostService;
 import com.connectin.domain.post.PostDTO;
 import com.connectin.utils.Response;
