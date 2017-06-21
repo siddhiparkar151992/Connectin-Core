3ec14947c3a89b8d7a8d2562c9acecc36651f437
siddhi_parkar <siddhiparkar15@gmail.com>
2017-06-21 13:27:39 +0530

    chnaged host

--- src/main/webapp/resources/static/app/config/url-config.service.ts
+++ src/main/webapp/resources/static/app/config/url-config.service.ts
@@ -6,14 +6,14 @@ export class UrlConfigService{
 	public httpUrl='https://';
 	public baseUrl;
 	public port = 8080;
-	public host ="connectin-social.herokuapp.com";
+	public host ="//connectin-social.herokuapp.com";
 	public apiBaseUrl="/connectin/api/";
 	public userFeedUrl ="user/feed/";	
 
 	constructor() {
 
 //		this.baseUrl = this.httpUrl +this.host+':'+this.port+this.apiBaseUrl;
-		this.baseUrl = this.httpUrl +this.host+this.apiBaseUrl;
+		this.baseUrl = this.host+this.apiBaseUrl;
 
 	}
 
